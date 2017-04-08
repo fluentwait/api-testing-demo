2bf5b3d0346168f58c799ba1b036121b11ffd1e1
master <sanithale@gmail.com>
2017-04-08 11:34:25 +0530

    Response Time Video

--- src/test/java/com/fluentWait/test/GoogleApiTest.java
+++ src/test/java/com/fluentWait/test/GoogleApiTest.java
@@ -9,10 +9,12 @@ import io.restassured.mapper.ObjectMapperType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.apache.http.HttpStatus;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Fluent Wait on 3/11/2017.
@@ -20,20 +22,21 @@ import java.util.List;
 public class GoogleApiTest {
 
     @Test(groups = {"demo", "google"})
-    public void googleApiTest(){
+    public void googleApiTest() {
         RequestSpecification requestSpecification = new RestAssuredConfiguration().getRequestSpecification();
         requestSpecification.queryParam("q", "turing");
         Response response =
                 new RestAssuredConfiguration().getResponse(requestSpecification, EndPoint.GOOGLE_API, HttpStatus.SC_OK);
         Google google = response.as(Google.class, ObjectMapperType.GSON);
         SoftAssert softAssert = new SoftAssert();
+        Assert.assertTrue(response.getTimeIn(TimeUnit.SECONDS) <= 10, "Response Time Is Not Within Limit");
         List<Item> items = google.getItems();
-        for (Item item : items){
+        for (Item item : items) {
             System.out.println(item.toString());
-            softAssert.assertTrue(!item.getId().equalsIgnoreCase(""),"ID IS BLANK");
+            softAssert.assertTrue(!item.getId().equalsIgnoreCase(""), "ID IS BLANK");
             VolumeInfo volumeInfo = item.getVolumeInfo();
             softAssert.assertTrue(!volumeInfo.getTitle().equalsIgnoreCase(""), "Title is Blank");
-       }
+        }
         softAssert.assertAll();
     }
 
